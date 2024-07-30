fd5a26f421471c8fe55770bd3bde6d7bb8b9f7c1
betester <ikramzulkarnaen02@gmail.com>
2024-07-30 14:38:30 +0700

    include raylib for linux

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-gcc -o main.exe main.c chess.c -I./raylib/include -L./raylib/lib -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc -o main.exe main.c chess.c -I./raylib-windows/include -L./raylib-windows/lib -lraylib -lopengl32 -lgdi32 -lwinmm
 
 if %ERRORLEVEL% NEQ 0 (
   echo compilation failed
---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -o main.o main.c chess.c -I./raylib-linux/include -L./raylib-linux/lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
